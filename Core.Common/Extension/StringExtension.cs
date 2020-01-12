09b55da9dc1df19ab3f2eaf17523f3b152b36622
Reynald BERRIOT <rberriot@vente-privee.com>
2020-01-13 00:49:29 +0100

    StringExtension ToJsonFormat with tests

---
+++ Core.Common/Extension/StringExtension.cs
@@ -0,0 +1,72 @@
+﻿using System.Text;
+
+namespace Core.Common.Extension
+{
+    public static class StringExtension
+    {
+        private const string INDENT_STRING = "  ";
+        public static string ToJsonFormat(this string str)
+        {
+            var indent = 0;
+            var quoted = false;
+            var sb = new StringBuilder();
+            for (var i = 0; i < str.Length; i++)
+            {
+                var ch = str[i];
+                switch (ch)
+                {
+                    case '{':
+                    case '[':
+                        sb.Append(ch);
+                        if (!quoted)
+                        {
+                            sb.AppendLine();
+                            Indent(sb, ++indent);
+                        }
+                        break;
+                    case '}':
+                    case ']':
+                        if (!quoted)
+                        {
+                            sb.AppendLine();
+                            Indent(sb, --indent);
+                        }
+                        sb.Append(ch);
+                        break;
+                    case '"':
+                        sb.Append(ch);
+                        bool escaped = false;
+                        var index = i;
+                        while (index > 0 && str[--index] == '\\')
+                            escaped = !escaped;
+                        if (!escaped)
+                            quoted = !quoted;
+                        break;
+                    case ',':
+                        sb.Append(ch);
+                        if (!quoted)
+                        {
+                            sb.AppendLine();
+                            Indent(sb, indent);
+                        }
+                        break;
+                    case ':':
+                        sb.Append(ch);
+                        if (!quoted)
+                            sb.Append(" ");
+                        break;
+                    default:
+                        sb.Append(ch);
+                        break;
+                }
+            }
+            return sb.ToString();
+        }
+
+        private static void Indent(StringBuilder sb, int count, string indent = INDENT_STRING)
+        {
+            for (int i = 0; i < count; i++)
+                sb.Append(indent);
+        }
+    }
+}
